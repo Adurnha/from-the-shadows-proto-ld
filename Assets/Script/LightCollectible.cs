7a52aefedaa63fbab5b6ab26980213618af8492d
Max Emsallem <emsallem.max@hotmail.fr>
2020-02-04 11:38:59 +0100

    Ajout des Collectibles d'ombre et de lumière

---
+++ Assets/Script/LightCollectible.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCollectible : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.tag == "Player" || other.tag == "DarkPlayer" || other.tag == "LightPlayer") && other.GetType() == typeof(BoxCollider))
+        {
+            if(other.GetComponent<PlayerController>().playerNumber == 1)
+                Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Script/ShadowCollectible.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowCollectible : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.tag == "Player" || other.tag == "DarkPlayer" || other.tag == "LightPlayer") && other.GetType() == typeof(BoxCollider))
+        {
+            if (other.GetComponent<PlayerController>().playerNumber == 2)
+                Destroy(this.gameObject);
+        }
+    }
+}
